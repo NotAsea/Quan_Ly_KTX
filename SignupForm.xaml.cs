6a8cc7fda8ffa4ab641deb1864d8adfb03a4c85d
Huu Hai <luonghaic1166@gmail.com>
2021-11-20 09:44:33 +0700

    Add Loggin and Signup

--- SignupForm.xaml.cs
+++ SignupForm.xaml.cs
@@ -10,29 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Quan_Ly_KTX
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-        }
-
-        private void Loggin_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Signup_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
