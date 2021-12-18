fe3820141a5f6be478e5942b8831eb0326caa5e2
Nguyễn Thị Lan Anh <AT150401@actvn.edu.vn>
2021-12-18 17:07:52 +0700

    optimizing part 21

--- Models/KTX_KMAContext.cs
+++ Models/KTX_KMAContext.cs
@@ -30,7 +30,9 @@ namespace Quan_Ly_KTX.Models
             if (!optionsBuilder.IsConfigured)
             {
                 //đây là url của m lúc m định push thì cmt cái này lại đỡ nhầm vs t
-               // optionsBuilder.UseSqlServer("Data Source=DESKTOP-J5215DU\\LANANH;Initial Catalog=KTX_KMA;Persist Security Info=True;User ID=sa;Password=123");
+                // optionsBuilder.UseSqlServer("Data Source=DESKTOP-J5215DU\\LANANH;Initial Catalog=KTX_KMA;Persist Security Info=True;User ID=sa;Password=123");
+                //đây là cái của t  lúc m định push thì bỏ cmt nó
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-Q5MH825\\HAILUONG;Initial Catalog=KTX_KMA;Persist Security Info=True;User ID=sa;Password=123");
             }
         }
 
