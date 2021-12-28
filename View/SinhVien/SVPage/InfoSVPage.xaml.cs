fa21e21888217ab2b081ceb34e9004fba6f5e166
Huu Hai <luonghaic1166@gmail.com>
2021-12-28 10:54:35 +0700

    done part 3

---
+++ View/SinhVien/SVPage/InfoSVPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Quan_Ly_KTX.Controller;
+using Quan_Ly_KTX.View;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Quan_Ly_KTX.SVPage
+{
+    /// <summary>
+    /// Interaction logic for EditInfoPage.xaml
+    /// </summary>
+    public partial class EditInfoPage : Page
+    {
+       
+        private int ID;
+        public EditInfoPage(InfoSV sv)
+        {
+            InitializeComponent();
+            this.DataContext = sv;
+            ID = sv.ID;
+         
+        }
+
+
+       
+    }
+}
